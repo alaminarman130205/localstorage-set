10c39aa9ff1884a0a90a8a0664e80a94711634d1
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-09-24 00:27:22 +0600

    localstorate added

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
 
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
+
+    const addTocart = (id) =>{
+        // console.log('item added ', id);
+       addToDb(id);
+    }
 
     return (
 
@@ -10,8 +16,9 @@ const Cosmetic = (props) => {
             <h2>Buy this: {name}</h2>
             <p>Only for ${price}</p>
             <p>It has id :{id}</p>
+            <button onClick={() => addTocart(id)}>add to cart</button>
         </div>
     );
-};
+};  
 
 export default Cosmetic;
